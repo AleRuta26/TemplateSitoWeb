781a8a891db2b51df462e65e2f123edc011b108d
Alessandro Ruta <aleruta2006@gmail.com>
2024-02-23 12:38:23 +0100

    Index image scrollbar implementation

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
 $(document).ready(function () {
+
         //Landscape
         //apertura menù
         $("#menu-icon").on('click', function () {
                 $(this).toggleClass("bi bi-list").toggleClass("bi bi-x-lg");
                 $(".menu").toggleClass("menu-disappear");
-        })
+        });
+
+        /*Scrollbar*/
+
+        var pos = 1;
+
+        $(".linea-scorrimento").on('input', function(){
+                pos = $(".linea-scorrimento").val();
+                $(".counter").fadeOut(100, function () {
+                        $(this).text(pos);
+                }).fadeIn(100);
+        });
+
+        
+        $(".bi-chevron-right").on('click', function () {
+                pos++;
+                pos = controllaPos(pos);
+                $(".linea-scorrimento").val(pos);
+                $(".counter").fadeOut(100, function () {
+                        $(this).text(pos);
+                }).fadeIn(100);
+        });
+        $(".bi-chevron-left").on('click', function () {
+                pos--;
+                pos = controllaPos(pos);
+                $(".linea-scorrimento").val(pos);
+                $(".counter").fadeOut(50, function () {
+                        $(this).text(pos);
+                }).fadeIn(50);
+        });
+
+        function controllaPos(n) {
+                if (n > 5) n = 1;
+                else if (n < 1) n = 5;
+                return n;
+        }
 });
 
 
