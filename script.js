c9521952d33161a716abde31593c6095b2502dc4
Alessandro Ruta <aleruta2006@gmail.com>
2024-02-23 10:11:39 +0100

    First Branch

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+        //Landscape
+        //apertura menù
+
+
+
+        $("#menu-icon").on('click', function () {
+                $(this).toggleClass("bi bi-list").toggleClass("bi bi-x-lg");
+                $(".menu").toggleClass("menu-disappear");
+        })
+
+
+});
+
+
