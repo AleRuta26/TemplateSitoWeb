ea747be3203442add9efbc25b887d75b444d57f8
Alessandro Ruta <aleruta2006@gmail.com>
2024-02-26 12:18:22 +0100

    Media Queries
    Added portrait support

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ $(document).ready(function () {
         $("#menu-icon").on('click', function () {
                 $(this).toggleClass("bi bi-list").toggleClass("bi bi-x-lg");
                 $(".menu").toggleClass("menu-disappear");
+                $("html").toggleClass("html-menu");
         });
 });
 
