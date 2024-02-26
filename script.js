84c6182a79a490e21e40cf50653917208e785110
Alessandro Ruta <aleruta2006@gmail.com>
2024-02-26 09:50:46 +0100

    Implemented jQuery as CDN

--- script.js
+++ script.js
@@ -3,32 +3,34 @@
 
 
 $(document).ready(function () {
+        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
         const swiper = new Swiper('.swiper', {
                 // Optional parameters
                 direction: 'horizontal',
                 loop: true,
                 spaceBetween: 30,
-              
+
                 // If we need pagination
                 pagination: {
-                  el: '.swiper-pagination',
-                  clickable: true,
+                        el: '.swiper-pagination',
+                        clickable: true,
                 },
-              
+
                 // Navigation arrows
                 navigation: {
-                  nextEl: '.swiper-button-next',
-                  prevEl: '.swiper-button-prev',
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
                 },
-        
-                
-                autoplay:{
-                        delay:2500,
-                        clickable:true,
-                        disableOnInteraction:false,
-                        pauseOnMouseEnter:true,
+
+
+                autoplay: {
+                        delay: 2500,
+                        clickable: true,
+                        disableOnInteraction: false,
+                        pauseOnMouseEnter: true,
                 },
-        
+
         });
         //apertura menù
         $("#menu-icon").on('click', function () {
